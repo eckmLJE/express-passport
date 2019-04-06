3657ccbe50022e559868c40a053a2bcf6f77082b
Lucas Eckman <lucas.eckman@gmail.com>
2019-04-06 17:45:05 -0400

    basic routes

--- src/index.js
+++ src/index.js
@@ -7,9 +7,35 @@ const app = express()
 app.use(cors())
 
 app.listen(process.env.PORT, () =>
-    console.log(`example app listening on port ${process.env.PORT}`)
+  console.log(`example app listening on port ${process.env.PORT}`)
 )
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/users', (req, res) => {
+  return res.send(database.users)
+})
+
+app.get('/users/:userId', (req, res) => {
+  const user = database.users.find(user => user.id == req.params.userId)
+  return res.send(user)
+})
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method')
+})
+
+app.post('/users/newuser', (req, res) => {
+  console.log('Received a new user post request')
+  return res.send('Received a new user post request')
+})
+
+const database = {
+  users: [],
+}
+
+const sampleUserNames = ['eckm', 'cbrue']
+let userId = 0
+
+sampleUserNames.forEach(userName => {
+  userId++
+  database.users.push({ id: userId, name: userName })
 })
