a5c2aab4d18c0fa05ae25263f220e26625c68e24
Lucas Eckman <lucas.eckman@gmail.com>
2019-04-08 08:53:32 -0400

    passporT

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import passport from 'passport'
+import passportLocal from 'passport-local'
 
 const app = express()
+
+const database = {
+  users: [],
+}
+
+const sampleUsers = [
+  { username: 'eckm', password: '12345' },
+  { username: 'cbrue', password: 'bubbelz' },
+]
+
+let userId = 0
+
+function createUser(user) {
+  userId++
+  database.users.push({
+    id: userId,
+    username: user.username,
+    password: user.password,
+  })
+}
+
+sampleUsers.forEach(user => {
+  createUser(user)
+})
+
+const LocalStrategy = passportLocal.Strategy
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) {
+        return done(err)
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' })
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' })
+      }
+      return done(null, user)
+    })
+  })
+)
 
 app.use(cors())
 
@@ -10,32 +55,20 @@ app.listen(process.env.PORT, () =>
   console.log(`example app listening on port ${process.env.PORT}`)
 )
 
+app.get('/', (req, res) => {
+  const user = req.user
+  return res.send(`${user ? user : 'not'} authenticated`)
+})
+
 app.get('/users', (req, res) => {
   return res.send(database.users)
 })
 
-app.get('/users/:userId', (req, res) => {
-  const user = database.users.find(user => user.id == req.params.userId)
-  return res.send(user)
-})
-
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method')
-})
-
-app.post('/users/newuser', (req, res) => {
-  console.log('Received a new user post request')
-  return res.send('Received a new user post request')
-})
-
-const database = {
-  users: [],
-}
-
-const sampleUserNames = ['eckm', 'cbrue']
-let userId = 0
-
-sampleUserNames.forEach(userName => {
-  userId++
-  database.users.push({ id: userId, name: userName })
-})
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: false,
+  })
+)
